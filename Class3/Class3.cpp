abe382ee56cbd013b70d1303744e3550ded0d411
Dramaturg1 <widowmakerNoOver@outlook.com>
2022-05-06 15:06:59 +0300

    добавил проект class3

---
+++ Class3/Class3.cpp
@@ -0,0 +1,176 @@
+﻿#define PI 3.14159
+
+#include <iostream>
+#include <tuple>
+#include <cmath>
+#include <limits>
+
+using namespace std;
+
+class Point {
+
+private:
+
+    int x, y;
+
+public:
+
+    void Set(int x, int y) {
+        this->x = x;
+        this->y = y;
+    };
+
+    int GetX() {
+        return x;
+    };
+
+    int GetY() {
+        return y;
+    };
+
+};
+
+class RectangularTriangle {
+
+private:
+
+    Point point1;
+    Point point2;
+    Point point3;
+    int count;
+
+public:
+
+    void Set(int x1, int y1, int x2, int y2, int x3, int y3, int count) {
+        point1.Set(x1, y1);
+        point2.Set(x2, y2);
+        point3.Set(x3, y3);
+        this->count = count;
+    };
+
+    tuple<double, double, double> GetLength() {
+        double a, b, c;
+        a = round((sqrt(pow(point2.GetX() - point1.GetX(), 2) + pow(point2.GetY() - point1.GetY(), 2))) * 100) / 100;
+        b = round((sqrt(pow(point3.GetX() - point2.GetX(), 2) + pow(point3.GetY() - point2.GetY(), 2))) * 100) / 100;
+        c = round((sqrt(pow(point1.GetX() - point3.GetX(), 2) + pow(point1.GetY() - point3.GetY(), 2))) * 100) / 100;
+        return { a, b, c };
+    };
+
+    bool Existance() {
+        auto [a, b, c] = GetLength();
+        if ((a + b > c) && (a + c > b) && (b + c > a)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    };
+
+    bool IsRect() {
+        if (Existance() == true) {
+            auto [A, B, C] = Angle();
+            if ((A == 90) || (B == 90) || (C == 90)) {
+                return true;
+            }
+            else {
+                return false;
+            };
+        };
+    };
+
+    double Perimeter() {
+        if (Existance() == true) {
+            auto [a, b, c] = GetLength();
+            return round((a + b + c) * 100) / 100;
+        }
+        else {
+            return 0;
+        };
+    };
+
+    double Square() {
+        if (Existance() == true) {
+            auto [a, b, c] = GetLength();
+            int p = Perimeter() / 2;
+            return round((sqrt(p * (p - a) * (p - b) * (p - c))) * 100) / 100;
+        }
+        else {
+            return 0;
+        }
+    };
+
+    tuple<double, double, double> Angle() {
+        if (Existance() == true) {
+            auto [a, b, c] = GetLength();
+            double angleA, angleB, angleC;
+            angleA = round(acos((b * b + c * c - a * a) / (2 * b * c)) * (180.0 / PI));
+            angleB = round(acos((a * a + c * c - b * b) / (2 * a * c)) * (180.0 / PI));
+            angleC = 180 - angleA - angleB;
+            return { angleA, angleB, angleC };
+        }
+        else {
+            return { 0, 0, 0 };
+        };
+
+    };
+
+    void Print() {
+        cout << "The first point: (" << point1.GetX() << "," << point1.GetY() << ")" << endl;
+        cout << "The second point: (" << point2.GetX() << "," << point2.GetY() << ")" << endl;
+        cout << "The third point: (" << point3.GetX() << "," << point3.GetY() << ")" << endl;
+        cout << "The number of triangle: " << count << endl;
+        cout << "Does this triangle exist? " << Existance() << endl;
+        if (Existance() == true) {
+            auto [a, b, c] = GetLength();
+            cout << "Sides of the triangle: " << a << ", " << b << ", " << c << endl;
+            auto [A, B, C] = Angle();
+            cout << "Angles of the triangle: " << A << ", " << B << ", " << C << endl;
+            cout << "Triangle's perimeter: " << Perimeter() << endl;
+            cout << "Triangle's square: " << Square() << endl;
+        };
+
+
+    };
+
+};
+
+int main() {
+    RectangularTriangle triangle[100];
+    cout << "How many triangles do you want to input?" << endl;
+    int N;
+    cin >> N;
+
+    for (int i = 0; i < N; i++) {
+        int x1, y1, x2, y2, x3, y3;
+        cout << "Input 3 points: " << endl;
+        cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
+        triangle[i].Set(x1, y1, x2, y2, x3, y3, i);
+        triangle[i].Print();
+        cout << endl;
+    };
+
+    long double maxS, minS;
+    maxS = -1;
+    minS = numeric_limits<long double>::max();
+    for (int i = 0; i < N; i++) {
+        if ((triangle[i].Existance() == true) && (triangle[i].IsRect() == true)) {
+            if (triangle[i].Square() > maxS) {
+                maxS = i;
+            };
+            if (triangle[i].Square() < minS) {
+                minS = i;
+            };
+        };
+    };
+
+    for (int i = 0; i < N; i++) {
+        if (i == maxS) {
+            cout << "Maximum square has a triangle with number " << i << ": " << triangle[i].Square() << endl;
+        };
+        if (i == minS) {
+            cout << "Minimum square has a triangle with mumber " << i << ": " << triangle[i].Square() << endl;
+        };
+    };
+
+    return 0;
+}
