61e102eb746b53b96e0d0510fe1feb9ac156be2d
Dramaturg1 <widowmakerNoOver@outlook.com>
2022-05-08 21:36:28 +0300

    обновил код в Inheritance

--- Inheritance/Inheritance.cpp
+++ Inheritance/Inheritance.cpp
@@ -1,1 +1,61 @@
-﻿
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+class Square {
+
+private:
+
+	double len;
+
+public:
+
+	Square(double len) {
+		this->len = len;
+	};
+
+	double GetLen() {
+		return len;
+	};
+
+	double Sq() {
+		return len * len;
+	};
+
+	double Perimeter() {
+		return 4 * len;
+	};
+
+	double Diagonal() {
+		return len * sqrt(2);
+	};
+
+	void Print() {
+		cout << "Side length of the square: " << GetLen() << endl;
+		cout << "Diagonal of the square: " << Diagonal() << endl;
+		cout << "Square area: " << Sq() << endl;
+		cout << "Square perimeter: " << Perimeter() << endl;
+	}
+};
+
+class SquarePrism : Square {
+
+
+private:
+
+	double H;
+
+public:
+
+	SquarePrism(double H, double len) :Square(len) {
+		this->H = H;
+	};
+
+
+
+};
+
+int main() {
+	return 0;
+};
